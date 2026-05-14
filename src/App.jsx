c65d9e69aebb8e5496e50a531ef17df4c6447dd1
HeeJeonLee <lhj3375610@gmail.com>
2026-05-14 10:07:06 +0900

    fix: 이메일 주소 제거 (SNS/랜딩페이지 전용)

--- src/App.jsx
+++ src/App.jsx
@@ -185,7 +185,6 @@ function App() {
               ["사업자등록번호", "653-90-02268"],
               ["대표전화", "1555-2137"],
               ["대표휴대폰", "010-5927-9205"],
-              ["이메일", "hejunl@hanmail.net"],
               ["주소", "경기도 수원시 팔달구 권광로 159, 1동 5층 502호(인계동, 수원프라자)"],
             ].map(([k, v]) => (
               <div key={k} className="flex gap-2">
@@ -221,8 +220,6 @@ function App() {
             <a href="#" className="hover:text-white transition">이용약관</a>
             <span>|</span>
             <a href="tel:15552137" className="hover:text-white transition">대표전화 1555-2137</a>
-            <span>|</span>
-            <a href="mailto:hejunl@hanmail.net" className="hover:text-white transition">hejunl@hanmail.net</a>
           </div>
           <p>새론금융대부중개 · 대부중개업 등록: 2026-수원-2324 · 사업자: 653-90-02268</p>
           <p>경기도 수원시 팔달구 권광로 159, 1동 5층 502호 (인계동, 수원프라자)</p>
